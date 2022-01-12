bdaaf27a608fdd902cd4e203fbf479f210051f3f
doniyor2109 <doniyor2109@gmail.com>
2022-01-12 11:21:22 +0500

    chore: Use custom sandbox.

--- .codesandbox/cra/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Button, ThemeProvider } from '@superdispatch/ui';
-import { render } from 'react-dom';
-
-render(
-  <ThemeProvider>
-    <Button />
-  </ThemeProvider>,
-  document.getElementById('root'),
-);
