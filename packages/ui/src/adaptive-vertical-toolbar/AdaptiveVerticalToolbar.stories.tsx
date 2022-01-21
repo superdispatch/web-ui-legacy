ae0b7633d63b9481b9c1ab60ff6d583e66145f48
doniyor2109 <doniyor2109@gmail.com>
2022-01-21 11:47:16 +0500

    feat(ui): Introduce AdaptiveVerticalToolbar.

--- packages/ui/src/adaptive-vertical-toolbar/AdaptiveVerticalToolbar.stories.tsx
+++ packages/ui/src/adaptive-vertical-toolbar/AdaptiveVerticalToolbar.stories.tsx
@@ -21,7 +21,7 @@ export const basic = () => (
           'Archive',
           'Delete',
         ].map((label) => ({ label, key: label }))}
-        render={(item) => <li>{item.label}</li>}
+        renderItem={(item) => <li>{item.label}</li>}
       />
     </CardContent>
   </Card>
@@ -41,7 +41,7 @@ export const customMore = () => (
           'Archive',
           'Delete',
         ].map((label) => ({ label, key: label }))}
-        render={(item) => <li>{item.label}</li>}
+        renderItem={(item) => <li>{item.label}</li>}
         moreElement={<li>··· More</li>}
       />
     </CardContent>
--- packages/ui/src/adaptive-vertical-toolbar/AdaptiveVerticalToolbar.tsx
+++ packages/ui/src/adaptive-vertical-toolbar/AdaptiveVerticalToolbar.tsx
@@ -39,7 +39,7 @@ export interface AdaptiveVerticalToolbarProps
   extends RefAttributes<HTMLDivElement>,
     Omit<ToolbarProps, 'children'> {
   items: AdaptiveVerticalToolbarItem[];
-  render: (item: AdaptiveVerticalToolbarItem) => ReactNode;
+  renderItem?: (item: AdaptiveVerticalToolbarItem) => ReactNode;
 
   moreElement?: ReactElement<{
     onClick: EventHandler<MouseEvent<HTMLElement>>;
@@ -47,103 +47,113 @@ export interface AdaptiveVerticalToolbarProps
 }
 
 export const AdaptiveVerticalToolbar: ForwardRefExoticComponent<AdaptiveVerticalToolbarProps> =
-  forwardRef(({ items, render, moreElement = defaultMoreElement }, ref) => {
-    const styles = useStyles();
-    const itemNodes = useRef<Array<null | HTMLElement>>([]);
-    const optionsButtonRef = useRef<HTMLDivElement>(null);
-    const [firstHiddenIdx, setFirstHiddenIdx] = useState(-1);
+  forwardRef(
+    (
+      {
+        items,
+        moreElement = defaultMoreElement,
+        renderItem = (item: AdaptiveVerticalToolbarItem) => item.label,
+      },
+      ref,
+    ) => {
+      const styles = useStyles();
+      const itemNodes = useRef<Array<null | HTMLElement>>([]);
+      const optionsButtonRef = useRef<HTMLDivElement>(null);
+      const [firstHiddenIdx, setFirstHiddenIdx] = useState(-1);
 
-    const menuItems = firstHiddenIdx === -1 ? [] : items.slice(firstHiddenIdx);
-    const [menuButtonNode, setMenuButtonRef] = useState<HTMLElement>();
+      const menuItems =
+        firstHiddenIdx === -1 ? [] : items.slice(firstHiddenIdx);
+      const [menuButtonNode, setMenuButtonRef] = useState<HTMLElement>();
 
-    const [rootNode, setRootNode] = useState<null | HTMLDivElement>(null);
+      const [rootNode, setRootNode] = useState<null | HTMLDivElement>(null);
 
-    useResizeObserver(rootNode, (node) => {
-      const rootRect = node.getBoundingClientRect();
-      const rootHeight = rootRect.bottom;
+      useResizeObserver(rootNode, (node) => {
+        const rootRect = node.getBoundingClientRect();
+        const rootHeight = rootRect.bottom;
 
-      const optionsButtonRect =
-        optionsButtonRef.current?.getBoundingClientRect();
-      const optionsButtonWidth = optionsButtonRect?.height || 0;
-      const maxBottomPosition = rootHeight - optionsButtonWidth;
+        const optionsButtonRect =
+          optionsButtonRef.current?.getBoundingClientRect();
+        const optionsButtonWidth = optionsButtonRect?.height || 0;
+        const maxBottomPosition = rootHeight - optionsButtonWidth;
 
-      const mountedNodes = itemNodes.current.filter(
-        (x): x is HTMLDivElement => x != null,
-      );
-      const hiddenIdx = mountedNodes.findIndex((itemNode, idx) => {
-        itemNode.removeAttribute('hidden');
+        const mountedNodes = itemNodes.current.filter(
+          (x): x is HTMLDivElement => x != null,
+        );
+        const hiddenIdx = mountedNodes.findIndex((itemNode, idx) => {
+          itemNode.removeAttribute('hidden');
 
-        const itemRect = itemNode.getBoundingClientRect();
+          const itemRect = itemNode.getBoundingClientRect();
 
-        // Ignore options button height when checking last item.
-        if (idx === mountedNodes.length - 1) {
-          return itemRect.bottom > rootHeight;
+          // Ignore options button height when checking last item.
+          if (idx === mountedNodes.length - 1) {
+            return itemRect.bottom > rootHeight;
+          }
+
+          return itemRect.bottom > maxBottomPosition;
+        });
+
+        if (hiddenIdx !== -1) {
+          mountedNodes.slice(hiddenIdx).forEach((itemNode) => {
+            itemNode.setAttribute('hidden', 'true');
+          });
         }
 
-        return itemRect.bottom > maxBottomPosition;
+        setFirstHiddenIdx(hiddenIdx);
       });
 
-      if (hiddenIdx !== -1) {
-        mountedNodes.slice(hiddenIdx).forEach((itemNode) => {
-          itemNode.setAttribute('hidden', 'true');
-        });
-      }
-
-      setFirstHiddenIdx(hiddenIdx);
-    });
-
-    return (
-      <Grid
-        spacing={1}
-        direction="column"
-        container={true}
-        wrap="nowrap"
-        className={styles.root}
-        ref={mergeRefs(ref, (node) => {
-          setRootNode(node);
-        })}
-      >
-        {items.map((item, idx) => (
-          <Grid
-            key={item.key}
-            item={true}
-            ref={(node) => {
-              itemNodes.current[idx] = node;
-            }}
-          >
-            {render(item)}
-          </Grid>
-        ))}
-
-        {menuItems.length > 0 && (
-          <Grid item={true} ref={optionsButtonRef} component="div">
-            {cloneElement(moreElement, {
-              onClick: ({ currentTarget }: MouseEvent<HTMLElement>) => {
-                setMenuButtonRef(currentTarget);
-              },
-            })}
-
-            <Menu
-              open={!!menuButtonNode}
-              anchorEl={menuButtonNode}
-              onClose={() => {
-                setMenuButtonRef(undefined);
+      return (
+        <Grid
+          spacing={1}
+          direction="column"
+          container={true}
+          wrap="nowrap"
+          className={styles.root}
+          ref={mergeRefs(ref, (node) => {
+            setRootNode(node);
+          })}
+        >
+          {items.map((item, idx) => (
+            <Grid
+              key={item.key}
+              item={true}
+              ref={(node) => {
+                itemNodes.current[idx] = node;
               }}
             >
-              {menuItems.map((item) => (
-                <MenuItem
-                  key={item.key}
-                  onClick={(event) => {
-                    item.onClick?.(event);
-                    setMenuButtonRef(undefined);
-                  }}
-                >
-                  {item.label}
-                </MenuItem>
-              ))}
-            </Menu>
-          </Grid>
-        )}
-      </Grid>
-    );
-  });
+              {renderItem(item)}
+            </Grid>
+          ))}
+
+          {menuItems.length > 0 && (
+            <Grid item={true} ref={optionsButtonRef} component="div">
+              {cloneElement(moreElement, {
+                onClick: ({ currentTarget }: MouseEvent<HTMLElement>) => {
+                  setMenuButtonRef(currentTarget);
+                },
+              })}
+
+              <Menu
+                open={!!menuButtonNode}
+                anchorEl={menuButtonNode}
+                onClose={() => {
+                  setMenuButtonRef(undefined);
+                }}
+              >
+                {menuItems.map((item) => (
+                  <MenuItem
+                    key={item.key}
+                    onClick={(event) => {
+                      item.onClick?.(event);
+                      setMenuButtonRef(undefined);
+                    }}
+                  >
+                    {item.label}
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Grid>
+          )}
+        </Grid>
+      );
+    },
+  );
