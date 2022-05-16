26f9a7dc992b46ae78dbd72bf8c44e7cdde871de
Diyorbek Sadullaev <diyorbek3377@gmail.com>
2022-05-16 13:38:32 +0200

    WIP

--- packages/forms/src/index.ts
+++ packages/forms/src/index.ts
@@ -5,4 +5,5 @@ export * from './enhanced/FormsProvider';
 export * from './enhanced/useFormikEnhanced';
 export * from './phone/FormikPhoneField';
 export * from './radio/FormikRadioGroupField';
+export * from './text/FormikMaxLengthTextField';
 export * from './text/FormikTextField';
