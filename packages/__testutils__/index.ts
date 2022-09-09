c6c9aab5e4eb4ab2a0be907bba02d316f3b9aeb5
doniyor2109 <doniyor2109@gmail.com>
2022-09-09 11:26:36 +0500

    Expose V5 Test utils APIs

--- packages/__testutils__/index.ts
+++ packages/__testutils__/index.ts
@@ -2,6 +2,6 @@ export * from './DateTestUtils';
 export * from './defer';
 export * from './renderComponent';
 export * from './renderCSS';
-export { renderStyles } from './renderStyles';
+export { extractCSS, renderStyles } from './renderStyles';
 export * from './renderTheme';
 export * from './serializers';
