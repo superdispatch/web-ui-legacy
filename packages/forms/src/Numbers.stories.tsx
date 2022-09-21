e1c2401fc51c64c6f2f6481a8034e1c3189fb696
Fedor Tokarev <90153051+tokarevfedor1991@users.noreply.github.com>
2022-09-21 19:48:08 +0500

    feat: Added FormikNumberField, FormikCurrencyField (#433)
    * feat:[CAR-3734] Added FormikNumberField, FormikCurrencyField
    
    * feat:[CAR-3734] Added FormikNumberField, FormikCurrencyField
    
    * chore: Updated snapshots changes.
    
    * feat:[CAR-3734] Added FormikNumberField, FormikCurrencyField
    
    * feat:[CAR-3734] Added FormikNumberField, FormikCurrencyField
    
    * chore: Updated snapshots changes.
    
    Co-authored-by: fv.tokarev <fv.tokarev@tensor.ru>
    Co-authored-by: github_actions <github-actions[bot]@users.noreply.github.com>

---
+++ packages/forms/src/Numbers.stories.tsx
@@ -0,0 +1,121 @@
+import { Alert } from '@material-ui/lab';
+import { Meta } from '@storybook/react';
+import { Button, Inline, Stack, useSnackbarStack } from '@superdispatch/ui';
+import { Box } from '@superdispatch/ui-lab';
+import { Form, FormikProvider } from 'formik';
+import { FormikCurrencyField, FormikNumberField, useFormikEnhanced } from '.';
+
+export default {
+  title: 'Recipes/Forms',
+  parameters: { playroom: { disable: true } },
+} as Meta;
+
+export const NumberFields = () => {
+  const { addSnackbar } = useSnackbarStack();
+
+  const formik = useFormikEnhanced<
+    {
+      quantity: string;
+      price: string;
+    },
+    Record<string, unknown>
+  >({
+    initialValues: {
+      quantity: '',
+      price: '',
+    },
+    onSubmit(values) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          if (!values.quantity || !values.price) {
+            reject(new Error('Invalid values'));
+          } else {
+            resolve(values);
+          }
+        }, 1000);
+      });
+    },
+    onSubmitSuccess() {
+      addSnackbar('Successfully!', { variant: 'success' });
+    },
+    onSubmitFailure(error) {
+      addSnackbar(error.message, { variant: 'error' });
+    },
+  });
+
+  const { status, resetForm, isSubmitting } = formik;
+
+  if (status.type === 'submitted') {
+    return (
+      <Stack>
+        <Alert severity="success">Done!</Alert>
+
+        <pre>{JSON.stringify(status.payload, null, 2)}</pre>
+
+        <Button
+          variant="contained"
+          onClick={() => {
+            resetForm();
+          }}
+        >
+          Reset
+        </Button>
+      </Stack>
+    );
+  }
+
+  return (
+    <FormikProvider value={formik}>
+      <Form>
+        <Box maxWidth="320px">
+          <Stack>
+            <FormikNumberField
+              name="quantity"
+              label="Quantity"
+              fullWidth={true}
+              inputProps={{
+                thousandSeparator: '',
+              }}
+              validate={(value) =>
+                value ? undefined : 'Please enter the quantity'
+              }
+            />
+
+            <FormikCurrencyField
+              name="price"
+              label="Price"
+              fullWidth={true}
+              validate={(value) =>
+                value ? undefined : 'Please enter the price'
+              }
+            />
+
+            {status.type === 'rejected' && (
+              <Alert severity="error">{status.payload.message}</Alert>
+            )}
+
+            <Inline>
+              <Button
+                type="submit"
+                variant="contained"
+                isLoading={isSubmitting}
+              >
+                Send
+              </Button>
+
+              <Button
+                onClick={() => {
+                  resetForm();
+                }}
+                variant="outlined"
+                disabled={isSubmitting}
+              >
+                Reset
+              </Button>
+            </Inline>
+          </Stack>
+        </Box>
+      </Form>
+    </FormikProvider>
+  );
+};
