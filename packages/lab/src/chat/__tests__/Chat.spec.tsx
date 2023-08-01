3f3210ee6f1806cd5f39634ffcabed62b06414fa
Roman Travnikov <roman@fromjimmy.com>
2023-08-01 11:55:59 +0300

    feat: [MSG-9][MSG-10] Update tests

--- packages/lab/src/chat/__tests__/Chat.spec.tsx
+++ packages/lab/src/chat/__tests__/Chat.spec.tsx
@@ -1,7 +1,36 @@
-import { render, screen } from '@testing-library/react';
+import { renderComponent } from '@superdispatch/ui-testutils';
+import { screen } from '@testing-library/react';
 import { Chat } from '../Chat';
+import { ChatMessage } from '../ChatMessage';
 
-test('Chat renders', () => {
-  render(<Chat>Chat</Chat>);
-  expect(screen.getByTestId('chat-container')).toMatchSnapshot();
+describe('Chat', () => {
+  it('Snapshot', () => {
+    renderComponent(<Chat>Chat</Chat>);
+    expect(screen.getByTestId('chat-container')).toMatchSnapshot();
+  });
+
+  it('Displays placeholder message when no messages are provided', () => {
+    renderComponent(<Chat />);
+
+    expect(
+      screen.getByText(/No new messages from Super Shipper./i),
+    ).toBeInTheDocument();
+  });
+
+  it('Renders provided messages', () => {
+    renderComponent(
+      <Chat>
+        <ChatMessage
+          variant="incoming"
+          author="Steve Trabajo"
+          role="dispatcher"
+          dateTime="2023-06-12 6:43PM"
+          text="Hello, Vin! ..."
+        />
+      </Chat>,
+    );
+
+    expect(screen.getByText('Steve Trabajo')).toBeInTheDocument();
+    expect(screen.getByText('Hello, Vin! ...')).toBeInTheDocument();
+  });
 });
