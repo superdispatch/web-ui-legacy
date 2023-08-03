e72c6e6e7013d623755e710c27fb722f743b039d
Roman Travnikov <travnikovrn@gmail.com>
2023-08-03 15:04:17 +0300

    feat: [MSG-9][MSG-10] Added chat component (#468)
    Co-authored-by: Roman Travnikov <roman@fromjimmy.com>
    Co-authored-by: github_actions <github-actions[bot]@users.noreply.github.com>

---
+++ packages/lab/src/chat/Chat.tsx
@@ -0,0 +1,55 @@
+import { Typography } from '@material-ui/core';
+import { Stack } from '@superdispatch/ui';
+import { Children, forwardRef } from 'react';
+import styled from 'styled-components';
+
+interface ChatProps {
+  children?: React.ReactNode | React.ReactNode[];
+  emptyText?: string;
+}
+
+const ChatContainer = styled('div')<{
+  isEmpty: boolean;
+  minWidth?: number;
+  maxWidth?: number;
+  minHeight?: number;
+  maxHeight?: number;
+}>(
+  ({ isEmpty, minWidth, maxWidth, minHeight, maxHeight }) => `
+  display: flex;
+  flex-direction: column-reverse;
+  overflow: auto;
+  justify-content: ${isEmpty ? 'center' : 'end'};
+  width: 100%;
+  height: 100%;
+  min-width: ${minWidth || 432}px;
+  max-width: ${maxWidth || 560}px;
+  min-height: ${minHeight || 368}px;
+  max-height: ${maxHeight || 696}px;
+  padding: 16px;
+  box-sizing: border-box;
+`,
+);
+
+function emptyPlaceholder(text: string): React.ReactNode {
+  return (
+    <Typography color="textSecondary" align="center">
+      {text}
+    </Typography>
+  );
+}
+
+export const Chat = forwardRef<HTMLDivElement, ChatProps>(
+  ({ children, emptyText = 'No new messages' }, ref) => {
+    const isEmpty = Children.toArray(children).length === 0;
+    return (
+      <ChatContainer data-testid="chat-container" isEmpty={isEmpty} ref={ref}>
+        <Stack space="small">
+          {isEmpty ? emptyPlaceholder(emptyText) : children}
+        </Stack>
+      </ChatContainer>
+    );
+  },
+);
+
+Chat.displayName = 'Chat';
---
+++ packages/lab/src/chat/ChatMessage.tsx
@@ -0,0 +1,42 @@
+import { Typography } from '@material-ui/core';
+import { Inline, Stack } from '@superdispatch/ui';
+import { Box } from '@superdispatch/ui-lab';
+import { forwardRef } from 'react';
+
+interface ChatMessageProps {
+  author: string;
+  role: string;
+  dateTime: string;
+  text: string;
+  variant: 'incoming' | 'outgoing';
+}
+
+export const ChatMessage = forwardRef<HTMLDivElement, ChatMessageProps>(
+  ({ author, role, dateTime, text, variant }, ref) => {
+    return (
+      <Stack
+        space="xxsmall"
+        align={variant === 'outgoing' ? 'right' : 'left'}
+        ref={ref}
+        data-testid="chat-message"
+      >
+        <Inline verticalAlign="center">
+          <Typography color="textPrimary">{author}</Typography>
+          <Typography color="textSecondary">{role}</Typography>
+          <Typography color="textSecondary">{dateTime}</Typography>
+        </Inline>
+        <Box
+          display="inline-block"
+          padding="xsmall"
+          marginTop="none"
+          borderRadius="medium"
+          backgroundColor={variant === 'outgoing' ? 'Blue50' : 'Silver200'}
+        >
+          <Typography color="textPrimary">{text}</Typography>
+        </Box>
+      </Stack>
+    );
+  },
+);
+
+ChatMessage.displayName = 'ChatMessage';
