23e6e0c02bae19d7e786044a1c0b3d767ec7d9cb
Umrzoq <umrzoq@superdispatch.com>
2023-08-06 17:03:59 +0500

    fix: next line reexport

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -25,6 +25,7 @@ export * from './props/CollapseProp';
 export * from './props/ResponsiveProp';
 export * from './props/SpaceProp';
 export * from './radio/RadioField';
+//prettier-ignore
 export * from './radio/RadioFieldCard';
 export * from './radio/RadioGroupField';
 export * from './responsive/CollapseBreakpoint';
