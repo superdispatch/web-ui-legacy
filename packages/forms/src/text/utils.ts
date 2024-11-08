c254ac9bf1addcf60095c5f239e66d14c41d848c
Durdona Bakhronova <durdona@superdispatch.com>
2024-11-08 18:06:32 +0500

    feat: [LM-1528] textfield select by searching (#536)

---
+++ packages/forms/src/text/utils.ts
@@ -0,0 +1,40 @@
+import {
+  Children,
+  Fragment,
+  isValidElement,
+  ReactElement,
+  ReactNode,
+} from 'react';
+
+export function getOptionsFromChildren(
+  children: ReactNode,
+): Array<{ value: unknown; label: string }> {
+  // TextField doesn't support Fragment
+  if (isFragment(children)) {
+    return [];
+  }
+
+  return Children.toArray(children)
+    .filter(
+      (
+        child,
+      ): child is ReactElement<{
+        value: unknown;
+        children?: ReactNode;
+      }> => isValidElement(child),
+    )
+    .map((child) => ({
+      value: child.props.value,
+      label: child.props.children?.toString() || '',
+    }));
+}
+
+function isFragment(
+  element: ReactNode,
+): element is ReactElement<{ children: ReactNode }> {
+  return isValidElement(element) && element.type === Fragment;
+}
+
+export function isSingleLetterOrNumber(key: string): boolean {
+  return key.length === 1 && /^[a-zA-Z0-9_.-]*$/.test(key);
+}
