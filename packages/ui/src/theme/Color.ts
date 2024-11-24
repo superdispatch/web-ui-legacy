be18fda013a1b348bc0f640cc737eef680e1a52d
MekhrangizY <mekhrangiz.yangiboeva@superdispatch.com>
2024-11-24 15:34:40 +0500

    migrated to CSS variables

--- packages/ui/src/theme/Color.ts
+++ packages/ui/src/theme/Color.ts
@@ -164,11 +164,175 @@ export enum Color {
   Yellow300 = '#E8671C',
   Yellow500 = '#B84807',
 }
+export enum ColorV2 {
+  //
+  // ???
+  //
+
+  White = '#fff',
+  White50 = 'rgba(255, 255, 255, 0.5)',
+  White40 = 'rgba(255, 255, 255, 0.4)',
+  White20 = 'rgba(255, 255, 255, 0.2)',
+  White10 = 'rgba(255, 255, 255, 0.1)',
+  White08 = 'rgba(255, 255, 255, 0.08)',
+  Transparent = 'rgba(0, 0, 0, 0)',
+
+  Black = '#000',
+  Black50 = 'rgba(0, 0, 0, 0.5)',
+  Black20 = 'rgba(0, 0, 0, 0.2)',
+
+  //Colors with 30% opacity
+  Dark30 = 'rgba(143, 148, 158, 0.3)',
+  Blue30 = 'rgba(0, 112, 245, 0.3)',
+  Green30 = 'rgba(3, 135, 47, 0.3)',
+  Red30 = 'rgba(229, 35, 13, 0.3)',
+  Silver30 = 'rgba(225, 229, 234, 0.3)',
+
+  //Colors with 10% opacity
+  Blue10 = 'rgba(0, 112, 245, 0.1)',
+  Red10 = 'rgba(195, 25, 9, 0.1)',
+
+  //
+  // Legacy Neutral Colors
+  //
+
+  /**@deprecated Use Dark100 */
+  Grey100 = '#8F949E',
+  /**@deprecated Use Dark200 */
+  Grey200 = '#6A707C',
+  /**@deprecated Use Dark300 */
+  Grey300 = '#5B6371',
+  /**@deprecated Use Dark400 */
+  Grey400 = '#323C4E',
+  /**@deprecated Use Dark450 */
+  Grey450 = '#222F44',
+  /**@deprecated Use Dark500 */
+  Grey500 = '#192334',
+
+  /** @deprecated */
+  Dark200 = '#6A707C',
+  /** @deprecated */
+  Dark400 = '#323C4E',
+  /** @deprecated */
+  Dark450 = '#222F44',
+
+  /** @deprecated */
+  Silver100 = '#F6F7F8',
+  /** @deprecated */
+  Silver300 = '#E8ECF0',
+
+  //
+  // Legacy Extended Palette
+  //
+
+  /** @deprecated */
+  Blue75 = '#CCE5FF',
+  /** @deprecated */
+  Blue100 = '#A8D1FF',
+  /** @deprecated */
+  Blue200 = '#66ADFF',
+  /** @deprecated */
+  Blue400 = '#0063DB',
+
+  /** @deprecated */
+  Green75 = '#C8F4D1',
+  /** @deprecated */
+  Green100 = '#90EAAE',
+  /** @deprecated */
+  Green200 = '#5DDA87',
+  /** @deprecated */
+  Green400 = '#1E8F46',
+
+  /** @deprecated */
+  Purple75 = '#DCDBF5',
+  /** @deprecated */
+  Purple100 = '#CBC8EE',
+  /** @deprecated */
+  Purple200 = '#A7A1E8',
+  /** @deprecated */
+  Purple400 = '#473ABB',
+
+  /** @deprecated */
+  Red75 = '#FDD9D3',
+  /** @deprecated */
+  Red100 = '#FDC2BA',
+  /** @deprecated */
+  Red200 = '#FE988B',
+  /** @deprecated */
+  Red400 = '#D9210D',
+
+  /** @deprecated */
+  Teal75 = '#BEEDF9',
+  /** @deprecated */
+  Teal100 = '#91E3F8',
+  /** @deprecated */
+  Teal200 = '#61D3EF',
+  /** @deprecated */
+  Teal400 = '#008DB8',
+
+  /** @deprecated */
+  Yellow75 = '#FFF1C2',
+  /** @deprecated */
+  Yellow100 = '#FFE494',
+  /** @deprecated */
+  Yellow200 = '#FFDC6B',
+  /** @deprecated */
+  Yellow400 = '#FFA91F',
+
+  //
+  // Neutral Colors
+  //
+
+  Dark50 = 'rgba(25, 35, 52, 0.5)',
+  Dark100 = 'var(--sd-dark100)',
+  Dark300 = 'var(--sd-dark300)',
+  Dark500 = 'var(--sd-dark500)',
+
+  Silver200 = 'var(--sd-silver200)',
+  Silver400 = 'var(--sd-silver400)',
+  Silver500 = 'var(--sd-silver500)',
+
+  //
+  // Extended Palette
+  //
+
+  Blue50 = 'var(--sd-blue50)',
+  Blue300 = 'var(--sd-blue300)',
+  Blue500 = 'var(--sd-blue500)',
+
+  Green50 = 'var(--sd-green50)',
+  Green300 = 'var(--sd-green300)',
+  Green500 = 'var(--sd-green500)',
+
+  Purple50 = 'var(--sd-purple50)',
+  Purple300 = 'var(--sd-purple300)',
+  Purple500 = 'var(--sd-purple500)',
+
+  Red50 = 'var(--sd-red50)',
+  Red300 = 'var(--sd-red300)',
+  Red500 = 'var(--sd-red500)',
+
+  Teal50 = 'var(--sd-teal50)',
+  Teal300 = 'var(--sd-teal300)',
+  Teal500 = 'var(--sd-teal500)',
+
+  Yellow50 = 'var(--sd-yellow50)',
+  Yellow300 = 'var(--sd-yellow300)',
+  Yellow500 = 'var(--sd-yellow500)',
+}
 
 export type ColorProp = keyof typeof Color;
+export type ColorPropV2 = keyof typeof ColorV2;
 
 export function isColorProp(name: unknown): name is ColorProp {
   return (
     typeof name == 'string' && Object.prototype.hasOwnProperty.call(Color, name)
+  );
+}
+
+export function isColorPropV2(name: unknown): name is ColorPropV2 {
+  return (
+    typeof name == 'string' &&
+    Object.prototype.hasOwnProperty.call(ColorV2, name)
   );
 }
--- packages/ui/src/theme/ThemeProvider.tsx
+++ packages/ui/src/theme/ThemeProvider.tsx
@@ -11,7 +11,10 @@ import {
 import { useConstant } from '@superdispatch/hooks';
 import { Rule, StyleSheet } from 'jss';
 import { ReactElement, ReactNode } from 'react';
-import { ThemeProvider as StyledThemeProvider } from 'styled-components';
+import {
+  createGlobalStyle,
+  ThemeProvider as StyledThemeProvider,
+} from 'styled-components';
 import { overrideAppBar } from '../app-bar/AppBarOverrides';
 import { overrideAutocomplete } from '../autocomplete/AutocompleteOverrides';
 import { overrideAvatar } from '../avatar/AvatarOverrides';
@@ -45,11 +48,70 @@ import {
 import { Color } from './Color';
 import { SuperDispatchTheme } from './SuperDispatchTheme';
 
+const GlobalStyles = createGlobalStyle`
+  :root {
+    --sd-blue50: #EBF4FF;
+    --sd-blue500: #0063DB;
+    --sd-blue300: #0070F5;
+    --sd-green300: #03872F;
+    --sd-green500: #007A29;
+    --sd-green50: #ECF9EF;
+    --sd-yellow300: #E8671C;
+    --sd-yellow500: #B84807;
+    --sd-yellow50: #FFF7DC;
+    --sd-red300: #E5230D;
+    --sd-red500:#C31909
+    --sd-red50:#FFEDEB
+    --sd-purple300:#6559CF
+    --sd-purple500:#473ABB
+    --sd-purple50:#EFEEFC
+    --sd-teal300:#007EAB
+    --sd-teal500:#00678A
+    --sd-teal50:#E3F6FC
+    --sd-silver500:#C4CDD5
+    --sd-silver200:#F3F5F8
+    --sd-silver400:#E1E5EA
+    --sd-dark100:#8F949E
+    --sd-dark300:#5B6371
+    --sd-dark500:#192334
+    --sd-pinkRed:#FE4A49
+  }
+
+    @media (prefers-color-scheme: dark) {
+  :root {
+   --sd-blue50:#0E2947
+    --sd-blue500:#55ADFF
+    --sd-blue300:#0070F5
+    --sd-green300:#03872F
+    --sd-green500:#5EE18A
+    --sd-green50:#092E16
+    --sd-yellow300:#E8671C
+    --sd-yellow500:#FCA542
+    --sd-yellow50:#33200A
+    --sd-red500:#FF6359
+    --sd-red50:#3D0A06
+    --sd-purple300:#6559CF
+    --sd-purple500:#BFA0FF
+    --sd-purple50:#262247
+    --sd-teal300:#007EAB
+    --sd-teal500:#31C7F5
+    --sd-teal50:#11333D
+    --sd-silver500:#47505B
+    --sd-silver200:#0D1017
+    --sd-silver400:#30373D
+    --sd-dark100:#6E7A8A
+    --sd-dark300:#AEB4BD
+    --sd-dark500:#FFFFFF
+    --sd-pinkRed:#FE4A49
+    }
+}     
+
+`;
+
 function createSuperDispatchTheme(): SuperDispatchTheme {
   const breakpoints = createBreakpoints({});
   const theme = createTheme({
     breakpoints,
-
     palette: {
       primary: {
         main: Color.Blue300,
@@ -161,7 +223,6 @@ export function ThemeProvider({
 }: ThemeProviderProps): ReactElement {
   const theme = useConstant(() => {
     const nextTheme = createSuperDispatchTheme();
-
     return !modifier ? nextTheme : modifier(nextTheme);
   });
 
@@ -174,6 +235,7 @@ export function ThemeProvider({
         <CssBaseline />
 
         <StyledThemeProvider theme={theme}>
+          <GlobalStyles />
           <ResponsiveContextProvider>
             <SnackbarStackProvider>{children}</SnackbarStackProvider>
           </ResponsiveContextProvider>
