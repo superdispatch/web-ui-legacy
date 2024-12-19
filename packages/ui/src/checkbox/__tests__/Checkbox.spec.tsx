b10446595caf772b837efabaf60315a5e6fbe037
MekhrangizY <mekhrangiz.yangiboeva@superdispatch.com>
2024-12-19 17:28:17 +0500

    fix: [PAYM-2321] fixed checkbox

--- packages/ui/src/checkbox/__tests__/Checkbox.spec.tsx
+++ packages/ui/src/checkbox/__tests__/Checkbox.spec.tsx
@@ -17,7 +17,7 @@ it('checks default props', () => {
         />
         <path
           d="M15.73 8l-.63.63c-1.43 1.43-2.94 3.05-4.37 4.5l-1.9-1.57-.7-.57L7 12.38l.7.57 2.53 2.09.63.52.58-.58c1.6-1.62 3.35-3.5 4.93-5.08l.63-.63L15.73 8z"
-          fill="var(--sd-white)"
+          fill="#fff"
         />
       </WithStyles(ForwardRef(SvgIcon))>,
       "color": "primary",
