10fad2b3b306660e250a3311b88c486f8ce2bba6
MekhrangizY <mekhrangiz.yangiboeva@superdispatch.com>
2024-12-19 17:28:17 +0500

    fix: [PAYM-2321] fixed checkbox

--- packages/ui/src/checkbox/CheckboxOverrides.tsx
+++ packages/ui/src/checkbox/CheckboxOverrides.tsx
@@ -1,5 +1,5 @@
 import { SvgIcon } from '@material-ui/core';
-import { ColorDynamic } from '../theme/Color';
+import { Color, ColorDynamic } from '../theme/Color';
 import { SuperDispatchTheme } from '../theme/SuperDispatchTheme';
 
 export function overrideCheckbox(theme: SuperDispatchTheme): void {
@@ -22,7 +22,7 @@ export function overrideCheckbox(theme: SuperDispatchTheme): void {
       <SvgIcon>
         <rect width="18" height="18" x="3" y="3" fill="currentColor" rx="2" />
         <path
-          fill={ColorDynamic.White}
+          fill={Color.White}
           d="M15.73 8l-.63.63c-1.43 1.43-2.94 3.05-4.37 4.5l-1.9-1.57-.7-.57L7 12.38l.7.57 2.53 2.09.63.52.58-.58c1.6-1.62 3.35-3.5 4.93-5.08l.63-.63L15.73 8z"
         />
       </SvgIcon>
