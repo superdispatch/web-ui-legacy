21b7210c94e994f7a6498e38d380afd7b9ef3d9f
Umrzoq <umrzoq@superdispatch.com>
2025-03-12 17:06:54 +0500

    feat(carrier): [CAR-6607] Reverted Button component

--- packages/lab/src/button/Button.tsx
+++ packages/lab/src/button/Button.tsx
@@ -1,4 +1,4 @@
-import CircularProgress from '@mui/material/CircularProgress';
+import { CircularProgress } from '@material-ui/core';
 import { Color, ColorDynamic } from '@superdispatch/ui';
 import {
   AriaAttributes,
