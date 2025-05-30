7f9975e4518686080035df128593084479c56b46
Yuriy Matskanyuk <yuriy.matskanyuk@gmail.com>
2025-05-30 16:14:36 +0500

    chore: Remove MUI 5 (#593)

--- packages/ui/src/theme/SuperDispatchTheme.ts
+++ packages/ui/src/theme/SuperDispatchTheme.ts
@@ -1,15 +1,10 @@
 import { Theme } from '@material-ui/core';
 import { SkeletonClassKey } from '@material-ui/lab';
-import { Theme as MaterialV5Theme } from '@mui/material/styles';
 
-declare module '@mui/styles/defaultTheme' {
-  export type DefaultTheme = MaterialV5Theme;
-}
 declare module '@material-ui/core/styles/overrides' {
   export interface ComponentNameToClassKey {
     MuiSkeleton: SkeletonClassKey;
   }
 }
 
-export type DefaultTheme = MaterialV5Theme;
 export type SuperDispatchTheme = Readonly<Required<Theme>>;
