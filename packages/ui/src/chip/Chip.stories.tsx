d5a12d33a084b70724a23ec4e6d49daa9a8517b6
Aliyev Doniyor <doniyor2109@gmail.com>
2025-09-23 14:28:42 +0500

    fix: Chip colors

--- packages/ui/src/chip/Chip.stories.tsx
+++ packages/ui/src/chip/Chip.stories.tsx
@@ -1,6 +1,6 @@
 import { Chip } from '@material-ui/core';
 import { PropsLink } from '@superdispatch/ui-docs';
-import { Inline } from '..';
+import { Inline, Stack } from '..';
 
 export default {
   title: 'Data Display/Chip',
@@ -11,16 +11,23 @@ export default {
   },
 };
 
+const colors = ['default', 'primary', 'secondary'] as const;
+
 export const examples = () => (
-  <Inline>
-    <Chip label="Basic" />
-    <Chip label="Disabled" disabled={true} />
-    <Chip label="Clickable" clickable={true} />
-    <Chip
-      label="Deletable"
-      onDelete={() => {
-        alert('Delete!');
-      }}
-    />
-  </Inline>
+  <Stack>
+    {colors.map((color) => (
+      <Inline key={color}>
+        <Chip label="Basic" color={color} />
+        <Chip label="Disabled" disabled={true} color={color} />
+        <Chip label="Clickable" clickable={true} color={color} />
+        <Chip
+          color={color}
+          label="Deletable"
+          onDelete={() => {
+            alert('Delete!');
+          }}
+        />
+      </Inline>
+    ))}
+  </Stack>
 );
